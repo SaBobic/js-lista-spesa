38fe9e230bef5bd6b35ffb16a25aa0e1d0cd5bff
SaBobic <fabrizioettori@gmail.com>
2022-06-28 15:09:57 +0200

    while loop method

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@
 3. Creo un ciclo for con condizione assoggettata alla length dell'array
 4. Nel ciclo for eseguo una concatenazione della variabile che deve contenere la stringa della lista della spesa
 5. Stampo la stringa in console
-*/
 
 // Dichiaro l'array
 const shoppingList = ["Uova", "Pane", "Latte", "Biscotti", "Pasta", "Affettati", "Formaggio", "Verdura", "Frutta"];
@@ -18,9 +17,46 @@ let shoppingListItems = "<ul>";
 
 // Creo un ciclo for con condizione assoggettata alla length dell'array
 for (let i = 0; i < shoppingList.length; i++){
+
     // Nel ciclo for eseguo una concatenazione della variabile che deve contenere la stringa della lista della spesa
     shoppingListItems += `<li>${shoppingList[i]}</li>`;
 };
+shoppingListItems += "</ul>";
+
+// Stampo la stringa in console
+console.log(shoppingListItems);
+*/
+
+//* PROCEDURA while
+
+/*
+1. Dichiaro l'array
+2. Dichiaro la variabile che conterrà per esteso la stringa della lista della spesa
+3. Dichiaro la variabile d'appoggio i
+4. Creo un ciclo while con condizione "i assoggettata alla length dell'array"
+5. Nel ciclo while eseguo una concatenazione della variabile che deve contenere la stringa della lista della spesa
+6. Per ultimo nel ciclo while, inserisco l'incremento i++ ad ogni giro
+7. Stampo la stringa in console
+*/
+
+// Dichiaro l'array
+const shoppingList = ["Uova", "Pane", "Latte", "Biscotti", "Pasta", "Affettati", "Formaggio", "Verdura", "Frutta"];
+
+// Dichiaro la variabile che conterrà per esteso la stringa della lista della spesa
+let shoppingListItems = "<ul>";
+
+// Dichiaro la variabile d'appoggio i
+let i = 0;
+
+// Creo un ciclo while con condizione "i assoggettata alla length dell'array"
+while (i < shoppingList.length){
+
+    // Nel ciclo while eseguo una concatenazione della variabile che deve contenere la stringa della lista della spesa
+    shoppingListItems += `<li>${shoppingList[i]}</li>`;
+
+    // Per ultimo nel ciclo while, inserisco l'incremento i++ ad ogni giro
+    i++
+}
 shoppingListItems += "</ul>";
 
 // Stampo la stringa in console
